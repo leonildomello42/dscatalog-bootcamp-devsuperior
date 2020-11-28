c9ff091337bd4c92e375e4c8e8414861ce0f2b46
leonildomello42 <leonildo.mello@gmail.com>
2020-11-28 11:02:07 -0300

    Modulo1 dscatalog - CRUD em springboot

---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/exceptions/ResourceNotFoundExcepetion.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscatalog.services.exceptions;
+
+public class ResourceNotFoundExcepetion extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundExcepetion(String msg) {
+		super(msg);
+	}
+	
+	
+
+}
